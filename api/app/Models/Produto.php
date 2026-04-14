60d89e0f05462afbaaeaa020b1e199dc94b7b62a
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 10:57:11 -0300

    feat: migration e model do produto criadas

---
+++ api/app/Models/Produto.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['nome', 'sku', 'preco', 'ativo', 'criado_por'])]
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    protected function casts(): array {
+        return [
+            'preco' => 'decimal:2',
+            'ativo' => 'boolean'
+        ];
+    }
+}
