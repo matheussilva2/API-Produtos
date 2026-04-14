976f27b5e7a50cb86392f8dbf12d8f0afdeab9a3
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 14:13:18 -0300

    feat: adicionado imagem_produto ao fillable do model produto

--- api/app/Models/Produto.php
+++ api/app/Models/Produto.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['nome', 'sku', 'preco', 'ativo', 'criado_por'])]
+#[Fillable(['nome', 'imagem_produto', 'sku', 'preco', 'ativo', 'criado_por'])]
 
 class Produto extends Model
 {
