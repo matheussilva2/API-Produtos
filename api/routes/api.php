1a315f12a9990dd024ccff8c8cd633e1edcd7b98
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 16:10:49 -0300

    feat: implementação da rota index produtos na api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'prefix' => 'auth'
-], function() {
+Route::group(['prefix' => 'auth'], function() {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
     Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+});
+
+Route::group(['prefix' => 'produtos'], function() {
+    Route::get('', [ProdutoController::class, 'index']);
 });
