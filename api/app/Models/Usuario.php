f4e6f9ef86f7c95ca1274b8ea97de95c9ed8d01a
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 10:47:39 -0300

    feat: nome alterado e adicionadas atributos de usuário ao model

--- api/app/Models/Usuario.php
+++ api/app/Models/Usuario.php
@@ -10,23 +10,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable(['nome', 'email', 'senha', 'cpf', 'telefone', 'tipo'])]
+#[Hidden(['senha', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            'senha' => 'hashed',
         ];
     }
 }
