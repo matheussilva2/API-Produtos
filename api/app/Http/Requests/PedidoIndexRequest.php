48c7a7073ecd375c3e44f4c5fb5c881e8db997c8
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 20:03:41 -0300

    feat: criada request para index de pedidos

---
+++ api/app/Http/Requests/PedidoIndexRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PedidoIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => ['nullable', Rule::in(['CRIADO', 'PAGO', 'CANCELADO']),],
+            'cidade_entrega' => 'nullable|string',
+            'estado_entrega' => 'nullable|string',
+            'per_page' => 'nullable|integer|min:1|max:100',
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'status.in' => 'O status do pedido precisa ser CRIADO, PAGO ou CANCELADO',
+            'per_page.min' => 'A limite de itens por página precisa ser maior do que zero',
+            'per_page.max' => 'O limite de itens por página é 100.',
+            'per_page.integer' => 'A quantidade de itens por página deve ter um número inteiro.',
+        ];
+    }
+}
