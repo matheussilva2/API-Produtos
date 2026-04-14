7fa927c449ada645aa098702d605136aad1782a9
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-14 16:10:35 -0300

    feat: implementação do index do produto controller

--- api/app/Http/Controllers/ProdutoController.php
+++ api/app/Http/Controllers/ProdutoController.php
@@ -12,6 +12,12 @@ class ProdutoController extends Controller
         $query = Produto::query();
 
         if($request->filled('nome')) {
+            //Verificando se é sqlite por causa dos testes
+            $isSqlite = config('database.default') === 'sqlite';
+            
+            $isSqlite ?
+            $query->where('nome', 'like', "%{$request->nome}%")
+            :
             $query->whereFullText('nome', $request->nome);
         }
 
@@ -19,8 +25,12 @@ class ProdutoController extends Controller
             $query->where('sku', $request->sku);
         }
 
+        if($request->filled('ativo')) {
+            $query->where('ativo', $request->ativo);
+        }
+
         $produtos = $query->orderBy('estoque', 'desc')
-            ->paginate($request->per_page || 10);
+            ->paginate($request->per_page?$request->per_page:10);
 
         return response()->json($produtos);
     }
