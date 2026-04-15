0eb5eacbd5962b929d1511d39dafdbd0d72cfbbe
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-15 10:55:17 -0300

    feat: criada request para criação de pedido

---
+++ api/app/Http/Requests/PedidoStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PedidoStoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'logradouro_entrega' => 'required|string',
+            'cidade_entrega' => 'required|string',
+            'estado_entrega' => 'required|string|size:2',
+            'itens' => 'required|array|min:1',
+            'itens.*.produto_id' => 'required|exists:produtos,id',
+            'itens.*.quantidade' => 'required|integer|min:1'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'logradouro_entrega.required' => 'O endereço de entrega é obrigatório.',
+            'cidade_entrega.required' => 'A cidade para entrega é obrigatória.',
+            'estado_entrega.required' => 'O estado para entrega é obrigatório.',
+            'estado_entrega.size' => 'O estado para entrega deve ser uma sigla. Ex.: "AL"',
+
+            'itens.required' => 'Informe pelo menos um produto para criar o pedido.',
+            'itens.*.produto_id.required' => 'O ID do produto é obrigatório.',
+            'itens.*.produto_id.exists' => 'Um ou mais produtos não foram encontrados.',
+
+            'itens.*.quantidade.required' => 'A quantidade do produto é obrigatória.',
+        ];
+    }
+}
