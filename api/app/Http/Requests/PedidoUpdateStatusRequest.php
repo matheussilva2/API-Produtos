5e20b19f108f74be3f97c3a95b11b816f4f584c3
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-15 11:23:23 -0300

    feat: criado request para atualização do status do pedido

---
+++ api/app/Http/Requests/PedidoUpdateStatusRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PedidoUpdateStatusRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => ['required', Rule::in(['CRIADO', 'PAGO', 'CANCELADO'])]
+        ];
+    }
+
+    public function messages(): array {
+        return [
+            'status.required' => 'O novo status é obrigatório',
+            'status.in' => 'O novo status deve ser CRIADO, PAGO ou CANCELADO'
+        ];
+    }
+}
