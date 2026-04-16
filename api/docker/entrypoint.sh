21541b5fe468c1e7fe27664612894723081cbddc
Matheus <matheusnascimentoeoq@gmail.com>
2026-04-16 16:20:35 -0300

    fix: corrigida condição para gerar chave JWT e APP KEY

--- api/docker/entrypoint.sh
+++ api/docker/entrypoint.sh
@@ -5,15 +5,16 @@ if [ ! -f /var/www/.env ]; then
     cp /var/www/.env.example /var/www/.env
 fi
 
-if [ -z "$(grep '^APP_KEY=' /var/www/.env | grep -v 'APP_KEY=$')" ]; then
+if [ -z "$(grep '^JWT_SECRET=.\+' /var/www/.env)" ]; then
+    echo "Gerando chave do JWT..."
+    php artisan jwt:secret --no-interaction --force
+fi
+
+if [ -z "$(grep '^APP_KEY=.\+' /var/www/.env)" ]; then
     echo "Gerando chave do app..."
     php artisan key:generate --no-interaction --force
 fi
 
-if [ -z "$(grep '^APP_KEY=' /var/www/.env | grep -v 'APP_KEY=$')" ]; then
-    echo "Gerando chave do JWT..."
-    php artisan jwt:secret --no-interaction --force
-fi
 
 php artisan storage:link
 
